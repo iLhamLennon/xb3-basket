4e8253c32ffe64e204a1571589fb7edcb17b75dc
iLham Lennon <ailhamwahyudi38@gmail.com>
2025-11-29 18:23:40 +0700

    modified:   db/connection.php

--- db/connection.php
+++ db/connection.php
@@ -3,17 +3,17 @@
     /*
 
     $host = "localhost";
-    $user = "avmcommu_prod";
-    $pass = "25justCORE*";
-    $base = "avmcommu_product";    
-    $port = 3306;
-
-    */
-
-    $host = "localhost";
     $user = "root";
     $pass = "";
     $base = "allsportsapi";
     $port = 3366;
+
+    */
+
+    $host = "localhost";
+    $user = "avmcommu_prod";
+    $pass = "25justCORE*";
+    $base = "avmcommu_product";    
+    $port = 3306;
 
     $conn = mysqli_connect($host, $user, $pass, $base, $port);
